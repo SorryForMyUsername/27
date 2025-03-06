e1bcc445fe5e3ab43eadd0c83b790486487afe69
Ilya <119899583+SorryForMyUsername@users.noreply.github.com>
2025-03-06 10:35:21 +0200

    Add files via upload

---
+++ Point.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _27
+{
+    internal class Point
+    {
+        /// <summary>
+        /// Поле Координата X
+        /// </summary>
+        int x;
+        /// <summary>
+        /// Поле Координата Y
+        /// </summary>
+        int y;
+
+        /// <summary>
+        /// Свойство Координата X
+        /// </summary>
+        public int X { get => x; set => x = value; }
+        /// <summary>
+        /// Свойство Координата Y
+        /// </summary>
+        public int Y { get => y; set => y = value; }
+
+        /// <summary>
+        /// Конструктор по умолчанию
+        /// </summary>
+        public Point()
+        {
+            X = 0;
+            Y = 0;
+        }
+
+        /// <summary>
+        /// Конструктор с двумя параметрами
+        /// </summary>
+        /// <param name="x">Координата X</param>
+        /// <param name="y">Координата Y</param>
+        public Point(int x, int y)
+        {
+            this.X = x;
+            this.Y = y;
+        }
+
+        /// <summary>
+        /// Метод для вывод значений координат
+        /// </summary>
+        /// <returns>Значения координат в виде строки</returns>
+        public override string ToString()
+        {
+            return $"x = {X, -5}y = {Y, -5}";
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _27
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Point[] points1 = InitPointArray();
+            OutputArray(points1);
+            Console.WriteLine();
+
+            Point[] points2 = InputPointArray(4);
+            OutputArray(points2);
+            Console.WriteLine();
+
+            List<Point> points3 = InputPointList(5);
+            OutputList(points3);
+            Console.WriteLine();
+
+            Console.ReadKey(true);
+        }
+
+        static Point[] InitPointArray()
+        {
+            return new Point[]{
+                new Point(1, 1),
+                new Point(-7, 10),
+                new Point(0, -5),
+                new Point(-1, -6),
+            };
+        }
+
+        static Point[] InputPointArray(int length)
+        {
+            Point[] points = new Point[length];
+            for (int i = 0; i < length; i++)
+            {
+                Console.Write($"points[{i}].X = ");
+                int x = int.Parse( Console.ReadLine() );
+                Console.Write($"points[{i}].Y = ");
+                int y = int.Parse(Console.ReadLine());
+                points[i] = new Point(x, y);
+            }
+            return points;
+        }
+
+        static List<Point> InputPointList(int length)
+        {
+            List<Point> points = new List<Point>();
+            for (int i = 0;i < length; i++)
+            {
+                Console.Write($"points[{i}].X = ");
+                int x = int.Parse(Console.ReadLine());
+                Console.Write($"points[{i}].Y = ");
+                int y = int.Parse(Console.ReadLine());
+                points.Add(new Point(x, y));
+            }
+            return points;
+        }
+
+        static void OutputArray(Point[] points)
+        {
+            Console.WriteLine("Массив точек:");
+            foreach (Point point in points)
+            {
+                Console.WriteLine($"  {point}");
+            }
+        }
+
+        static void OutputList(List<Point> points)
+        {
+            Console.WriteLine("Лист точек:");
+            foreach (Point point in points)
+            {
+                Console.WriteLine($"  {point}");
+            }
+        }
+    }
+}
